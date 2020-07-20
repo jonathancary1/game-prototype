f3ee1852a8d16e95f0ef93a5a2e5d8bd31e0c5dd
Jonathan Cary <jonathancary1@gmail.com>
2020-07-21 07:59:46 +0900

    Added Leg Demo Scene

---
+++ Assets/Scripts/Character/Demo/LegSegmentManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LegSegmentManager : MonoBehaviour
+{
+    public LegManager Leg;
+
+    public Transform Anchor;
+    public Transform Target;
+    public Transform Forward;
+
+    private void Update()
+    {
+        Vector3 anchor = Anchor.position;
+        Vector3 target = Target.position;
+        Vector3 relative = Forward.position - Vector3.Project(Forward.position, target - anchor);
+        Vector3 upwards = Vector3.ProjectOnPlane(relative, Vector3.up);
+        Leg.SetLeg(new Segment(anchor, target, upwards), Vector3.up);
+    }
+}
